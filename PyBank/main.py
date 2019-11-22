ebc2e3112eaba48495ad57c0f9d9b950c943fc98
Joshua Cohen <jmcohen77@gmail.com>
2019-11-21 18:00:39 -0600

    updated code

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,54 +14,55 @@ greatest_decrease = 0
 greatest_decrease_month = 0
 
 #Set Path for File
-
+csvpath = "budget.data.csv"
 
 #Open and Read CSV File
 
 
-    #CSV Reader Specifies Delimniter & Variable that Holds Contents
-    csvreader = csv.reader(csvfile, delimiter=",")
+    #CSV Reader Specifies Delimiter & Variable that Holds Contents
+    with open(csvpath, "r") as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=",")
 
     #Read Header Row First
-    csv_header = next(csvreader)
-    row = next(csvreader)
+        csv_header = next(csvreader)
+        row = next(csvreader)
 
     #Calculate Total Number of Months, Net Amount of P&L, and Set Variables for Rows
-    previous_row = int(row[1])
-    total_months += 1
-    net_amount += int(row[1])
-    greatest_increase = int(row[1])
-    greatest_increase_month = row[0]
+        previous_row = int(row[1])
+        total_months += 1
+        net_amount += int(row[1])
+        greatest_increase = int(row[1])
+        greatest_increase_month = row[0]
 
 #Read Each Row of Data After the Header
-for row in csvreader:
+    for row in csvreader:
     #Calculate Total Number of Months Included in Dataset
-    total_months += 1
+        total_months += 1
     #Calculate Net Amount of P&L over Entire Period
-    net_amount += int(row[1])
+        net_amount += int(row[1])
 
-    #Calculate Change from CUrrent Month to Previous Month
-
-
-
-
-
+    #Calculate Change from Current Month to Previous Month
+        revenue_change = int(row[1]) - previous_row
+        monthly_change.append(revenue_change)
+        previous_row = int(row[1])
+        month_count.append(row[0])
 
     #Calculate the Greatest Increase
-
-
-
-
-
+        if int(row[1]) > greatest_increase:
+            greatest_increase = int(row[1])
+            greatest_increase_month = row[0]
+        
     #Calculate the Greatest Decrease
-
-
-
+        if int(row[1]) < greatest_decrease:
+            greatest_decrease = int(row[1])
+            greatest_decrease_month = row[0]
 
     #Calculate the Average & The Date
-    average_change = sum(monthly_change)/ len(monthly_change)
+        average_change = sum(monthly_change)/ len(monthly_change)
 
-    highest = max(monthly_change)
-    lowest = min(monthly_change)
+        highest = max(monthly_change)
+        lowest = min(monthly_change)
 
     #Now What???
+
+    #Print Analysis?
